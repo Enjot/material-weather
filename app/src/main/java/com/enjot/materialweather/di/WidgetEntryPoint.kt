f23a0cb0938ed7ca9aab8556ec0efe76e53c90b5
Enjot <jakubnahacz@gmail.com>
2024-04-13 17:13:53 +0200

    added initial code to widget feature using compose glance

---
+++ app/src/main/java/com/enjot/materialweather/di/WidgetEntryPoint.kt
@@ -0,0 +1,12 @@
+package com.enjot.materialweather.di
+
+import com.enjot.materialweather.domain.usecase.GetLocalWeatherUseCase
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@EntryPoint
+@InstallIn(SingletonComponent::class)
+interface WidgetEntryPoint {
+    fun getLocalWeatherUseCase(): GetLocalWeatherUseCase
+}
