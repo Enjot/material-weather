f76a5d61db2813dc0429aa890faa1b62acc36cff
Enjot <jakubnahacz@gmail.com>
2024-07-27 13:58:51 +0200

    chore: minor changes

--- app/src/main/java/com/enjot/materialweather/presentation/navigation/AppNavigation.kt
+++ app/src/main/java/com/enjot/materialweather/presentation/navigation/AppNavigation.kt
@@ -7,11 +7,9 @@ import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.IntOffset
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import androidx.navigation.toRoute
 import com.enjot.materialweather.presentation.ui.screen.daily.DailyScreen
 import com.enjot.materialweather.presentation.ui.screen.overview.OverviewScreen
