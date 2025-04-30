18e3b7a30538c369c9dcd534deb87e59cb9859f1
Jakub Nahacz <jakubnahacz@gmail.com>
2025-04-30 18:58:02 +0200

    feat: tests for new SettingsManager refactor: removed tests for removed classes

---
+++ app/src/test/java/com/enjot/materialweather/fakes/SettingsRepositoryFake.kt
@@ -0,0 +1,27 @@
+package com.enjot.materialweather.fakes
+
+import com.enjot.materialweather.domain.model.Settings
+import com.enjot.materialweather.domain.repository.SettingsManager
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+
+class SettingsMangerFake : SettingsManager {
+
+    val userPreferences = MutableStateFlow(Settings())
+
+    override fun settingsStateFlow(): Flow<Settings> {
+        return userPreferences
+    }
+
+    override suspend fun toggleBackgroundUpdates() {
+        userPreferences.update {
+            it.copy(backgroundUpdatesEnabled = !it.backgroundUpdatesEnabled)
+        }
+    }
+
+    override suspend fun setBackgroundUpdatesRepeatInterval(repeatInterval: Long) {
+        userPreferences.update { it.copy(backgroundUpdatesRepeatInterval = repeatInterval) }
+    }
+
+}
